972049870e01ac480ea135dc502a7edfbe4a70d1
Gerard Serra <gerard.serra@vasscompany.com>
2025-11-16 18:54:20 +0100

    CharactersRepository added with in-memory

---
+++ Rick&MortyApp/Rick&MortyApp/Core/Cache/ResponseCache.swift
@@ -0,0 +1,8 @@
+//
+//  ResponseCache.swift
+//  Rick&MortyApp
+//
+//  Created by Gerard Serra Rodriguez on 16/11/25.
+//
+
+import Foundation
---
+++ Rick&MortyApp/Rick&MortyApp/Data/Repository/CharactersRepository.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersRepository.swift
+//  Rick&MortyApp
+//
+//  Created by Gerard Serra Rodriguez on 16/11/25.
+//
+
+import Foundation
