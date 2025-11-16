f6d6e74abb29cafe1b8f0be61790023a9730842b
Gerard Serra <gerard.serra@vasscompany.com>
2025-11-16 18:05:28 +0100

    Rick & Morty app

---
+++ Rick&MortyApp/Rick&MortyApp/Rick_MortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyAppApp.swift
+//  Rick&MortyApp
+//
+//  Created by Gerard Serra Rodriguez on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
